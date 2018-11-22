8d2f37a261ac6b82e115237113a14ee18970f449
buengese <buengese@gmail.com>
2018-11-22 22:04:33 +0100

    Bla

--- src/assignment4_camera_calibration/src/image_processing.py
+++ src/assignment4_camera_calibration/src/image_processing.py
@@ -21,7 +21,7 @@ from matplotlib import pyplot as plt
 class image_converter:
 
   def __init__(self):
-    self.image_pub = rospy.Publisher("/image_processing/bin_img2",Image, queue_size=1)
+    self.image_pub = rospy.Publisher("/image_processing/bin_img",Image, queue_size=1)
 
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/camera/color/image_raw",Image,self.callback, queue_size=1)
@@ -39,27 +39,28 @@ class image_converter:
     # make it gray
     gray=cv2.cvtColor(cv_image_cropped, cv2.COLOR_BGR2GRAY)
 
+    # turn image black
     bi_gray_max = 255
-    bi_gray_min = 245
+    bi_gray_min = 240
     ret,thresh1=cv2.threshold(gray, bi_gray_min, bi_gray_max, cv2.THRESH_BINARY);
 
-    # publish the final image
+    # publish the black and white image
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(thresh1, "mono8"))
     except CvBridgeError as e:
       print(e)
 
-        ##Getting the points from image##
     image_points = np.zeros((6,2));
 
+    # Each loop searches for one point because our points are very of center in the area search by each loop is very different
+    # top left point
     for i in range (0,220):
         for j in range(0,320):
                   if (thresh1[i,j] >= 200):
                       image_points[0,0]=j #
                       image_points[0,1]=i
 
-
-
+    # middle left
     for i in range (220,300):
         for j in range (0,320):
                if (thresh1[i,j] >= 200):
@@ -67,30 +68,28 @@ class image_converter:
                       image_points[1,1]=i
 
 
-
+    # bottem left
     for i in range(300,420):
        for j in range(0,320):
                  if (thresh1[i,j] >= 200):
                       image_points[2,0]=j
                       image_points[2,1]=i
 
-
-
+    # top right                  
     for i in range(0,220):
        for j in range(320,640):
                  if (thresh1[i,j] >= 200):
                       image_points[3,0]=j
                       image_points[3,1]=i
 
-
-
+    # middle right                  
     for i in range(220,300):
         for j in range(320,640):
                   if (thresh1[i,j] >= 200):
                       image_points[4,0]=j
                       image_points[4,1]=i
 
-
+    # bottem right
     for i in range (300,420):
         for j in range(320,640):
                if (thresh1[i,j] >= 200):
@@ -114,13 +113,14 @@ class image_converter:
     p2 = 0
     dist_coeffs = np.zeros((4,1))
     dist_coeffs[:,0] = np.array([[k1, k2, p1, p2]])
-    # far to close, left to right (order of discovery) in cm
+
     obj_points = np.zeros((6,3,1))
+    # left far to close, right far to close
     obj_points[:,:,0] = np.array([[0, 0, 0],
-                                  [41.7, 0, 0],
-                                  [0, 58.3, 0],
-                                  [40.0, 30.0, 0],
                                   [0, 30.0, 0],
+                                  [0, 58.3, 0],
+                                  [41.7, 0, 0],
+                                  [40.0, 30.0, 0],
                                   [40.0, 58.4, 0]])
     retval, rvec, tvec = cv2.solvePnP(obj_points, image_points,camera_mat, dist_coeffs)
     print 'rvec \n' , rvec
@@ -136,14 +136,16 @@ class image_converter:
     sy = math.sqrt(rmat[0,0] * rmat[0,0] +  rmat[1,0] * rmat[1,0]);
     singular = sy < 1e-6; # If
     if (~singular):
-         x = math.atan2(-rmat[2,1] , rmat[2,2]);
-         y = math.atan2(-rmat[2,0], sy);
-         z = math.atan2(rmat[1,0], rmat[0,0]);
+         roll = math.atan2(-rmat[2,1] , rmat[2,2]);
+         pitch = math.atan2(-rmat[2,0], sy);
+         yaw = math.atan2(rmat[1,0], rmat[0,0]);
     else:
-         x = math.atan2(-rmat[1,2], rmat[1,1]);
-         y = math.atan2(-rmat[2,0], sy);
-         z = 0;
-    print 'x,y,z', inv_tvec[0]/100, inv_tvec[1]/100, inv_tvec[2]/100
+         roll = math.atan2(-rmat[1,2], rmat[1,1]);
+         pitch = math.atan2(-rmat[2,0], sy);
+         yaw = 0;
+
+    print 'position (x, y, z)', inv_tvec[0]/100, inv_tvec[1]/100, inv_tvec[2]/100
+    print 'rotation (yaw, pitch, roll)', yaw, pitch, roll
 
 
 def main(args):
