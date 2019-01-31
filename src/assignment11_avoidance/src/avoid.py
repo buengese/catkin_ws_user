5959b6067c32a1887e1341f4d56cfc144f3402dd
buengese <buengese@gmail.com>
2019-01-31 14:12:40 +0100

    Assignment 11: done

--- src/assignment11_avoidance/src/avoid.py
+++ src/assignment11_avoidance/src/avoid.py
@@ -7,43 +7,59 @@ import matplotlib.pyplot as plt
 from nav_msgs.msg import Odometry
 from matplotlib.patches import Wedge
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import UInt8, Int16
+
+obstacle_track_threshold = 0.10
+obstacle_distance_threshold = 1.
 
 plot = True
 ax = None
 mat = None
 pos = None
-yaw = None
 
-wall1_angle = np.pi
+lane_pub = None
+speed_pub = None
+
+def getClosestPoint(pos, laneID):
+	if laneID == 1:
+		p1 = (1.90, .80)
+		p2 = (4.05, .79)
+		p3 = (1.95, 3.58)
+		p4 = (4.05, 3.58)
+		c1 = (1.90, 2.22) 
+		c2 = (4.15, 2.15)
+		r1 = 1.27
+		r2 = 1.31 
+	if laneID == 2:
+		p1 = (1.95, .48)
+		p2 = (4.04, .48)
+		p3 = (1.95, 3.88)
+		p4 = (4.05, 3.88)
+		c1 = (1.90, 2.22)
+		c2 = (4.15, 2.15)
+		r1 = 1.60
+		r2 = 1.65
+	if (pos[0] < p1[0]):
+		angle = math.atan((pos[1] - c1[1])/(pos[0] - c1[0]))
+		return (c1[0] + r1 * math.cos(angle), c1[1] + r1 * math.sin(angle))
+	elif (pos[0] < c2[0]) and (pos[1] < c2[1]):
+		return (pos[0], p1[1])
+	elif (pos[0] < c2[0]) and (pos[1] >= c2[1]):
+		return (pos[0], p3[1])
+	else:
+		angle = math.atan((pos[1] - c2[1])/(pos[0] - c2[0]))
+		return (c2[0] + r2 * math.cos(angle), c2[1] + r2 * math.sin(angle))
 
 def on_scan(scan_msg):
-	'''radius = np.asarray(scan_msg.ranges)
-	angles = np.arange(scan_msg.angle_min, scan_msg.angle_max + scan_msg.angle_increment / 2, scan_msg.angle_increment)
+	if mat is None:
+		return
 
-	mask_fin = np.isfinite(radius)  # only consider finite radii
-	#angle_spread_method1 = np.pi / 8  # size of cone to consider
-
-	#mask_angle1 = np.logical_or(angles < (angle_spread_method1 - wall1_angle), angles > (wall1_angle - angle_spread_method1))
-	#mask_front = np.logical_and(mask_fin, mask_angle1)
-
-	masked_angles_front = angles[mask_fin]
-	masked_radius_front = radius[mask_fin]
-
-	x = np.cos(masked_angles_front) * masked_radius_front
-	y = np.sin(masked_angles_front) * masked_radius_front
-	ones = np.ones(len(x))
-	zeros = np.zeros(len(x))
-	points_front = np.column_stack((x, y, zeros, ones))
-	for p, i in enumerate(points_front):
-		world = np.dot(mat, p)
-		print(world)'''
-	#points_out = points_out[:,:2]
-	#print(points_out)
 	ranges = scan_msg.ranges
 	out = np.zeros((360, 2))
 
 	for inx, i in enumerate(ranges):
-		print(inx)
+		if inx > 65 and not (inx > 295):
+			continue
 		inxr = inx*np.pi/180
 		x = np.cos(inxr) * i
 		y = np.sin(inxr) * i
@@ -51,6 +67,36 @@ def on_scan(scan_msg):
 		world = np.dot(mat, auto_vector)
 		out[inx, :] = world[:2, 0].reshape(1 ,2)
 
+	out[np.isnan(out)] = 42
+	out[np.isinf(out)] = 42
+
+	closestPoints1 = np.array([getClosestPoint(o, 1) for o in out])
+	closestPoints2 = np.array([getClosestPoint(o, 2) for o in out])
+	distanceToTrack1 = np.array([np.linalg.norm(closestPoints1[i] - out[i]) for i in range(len(out))])
+	distanceToTrack2 = np.array([np.linalg.norm(closestPoints2[i] - out[i]) for i in range(len(out))])
+	closestPoints1 = closestPoints1[distanceToTrack1 < obstacle_track_threshold]
+	closestPoints2 = closestPoints2[distanceToTrack2 < obstacle_track_threshold]
+	closestPointCar = getClosestPoint([pos.x, pos.y], 1)
+	distanceToCar1 = np.array([np.linalg.norm(closestPoints1[i] - closestPointCar) for i in range(len(closestPoints1))])
+	distanceToCar2 = np.array([np.linalg.norm(closestPoints2[i] - closestPointCar) for i in range(len(closestPoints2))])
+	if (len(distanceToCar1) != 0 and len(distanceToCar2) != 0 and np.min(distanceToCar1) < obstacle_distance_threshold and np.min(distanceToCar2) < obstacle_distance_threshold):
+		speed_pub.publish(0)
+	elif (len(distanceToCar1) != 0 and np.min(distanceToCar1) < obstacle_distance_threshold):
+		lane_pub.publish(2)
+	elif (len(distanceToCar2) != 0 and np.min(distanceToCar2) < obstacle_distance_threshold):
+		lane_pub.publish(1)
+	else:
+		speed_pub.publish(200)
+
+	if plot:
+		prepareVisualization()
+		ax.scatter(*out.T, color='g')
+		ax.scatter(*closestPoints1.T, color='r')
+		ax.scatter(*closestPoints2.T, color='r')
+		plt.show(block=False)
+		rospy.sleep(0.1)  # sleep to avoid threading issues with plotting
+
+def prepareVisualization():
 	ax.cla()
 	angle = 90
 	theta1, theta2 = angle, angle + 180
@@ -76,23 +122,22 @@ def on_scan(scan_msg):
 	ax.plot([1.95,4.05], [3.52, 3.52], color='b')
 	ax.plot(pos.x, pos.y, marker='o')
 	#ax.plot([pos.x, pos.x + 0.2 * math.cos(yaw)], [pos.y, pos.y + 0.2 * math.sin(yaw)], color='g')
-	ax.scatter(*out.T, color='g')
-
-	plt.show(block=False)
-	rospy.sleep(0.1)  # sleep to avoid threading issues with plotting
 
 def on_odom(msg):
 	global mat
 	global pos
-	global yaw
 	pos = msg.pose.pose.position
 	(r, p, y) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
 	mat = np.matrix([[np.cos(y), -np.sin(y), 0, pos.x], [np.sin(y), np.cos(y), 0, pos.y], [0, 0, 1, 0], [0, 0, 0, 1]])
 
 def main():
 	global ax
-	rospy.init_node("collision")
+	global lane_pub
+	global speed_pub
+	rospy.init_node("obstacle_detect")
 	fig, ax = plt.subplots()
+	lane_pub = rospy.Publisher("/lane", UInt8, queue_size=1, latch=True)
+	speed_pub = rospy.Publisher("/manual_control/speed", Int16, queue_size=1, latch=True)
 	rospy.Subscriber("/scan", LaserScan, on_scan, queue_size=1)
 	pos_sub = rospy.Subscriber("/localization/odom/7", Odometry, on_odom, queue_size=1)
 	if plot:
---
+++ src/assignment11_avoidance/src/gps.py
@@ -0,0 +1,155 @@
+import rospy
+import sys
+import signal
+import math
+import matplotlib.pyplot as plt
+import tf
+from matplotlib.patches import Wedge
+from sympy import *
+import numpy as np
+from sympy.geometry import *
+from nav_msgs.msg import Odometry
+from std_msgs.msg import UInt8
+
+steering_pub = None
+plot = False
+
+laneID = 1
+
+# terrible code ahead
+def getClosestPoint(pos, distance, laneID):
+	if laneID == 1:
+		p1 = (1.90, .80)
+		p2 = (4.05, .79)
+		p3 = (1.95, 3.58)
+		p4 = (4.05, 3.58)
+		c1 = (1.90, 2.22) 
+		c2 = (4.15, 2.15)
+		r1 = 1.30
+		r2 = 1.35 
+		# Verwendete Koordinaten entsprechen nicht ganz dem was wir in dem Bild gemessen haben fuehren so aber zu deutlich besseren Ergebnissen
+	if laneID == 2:
+		p1 = (1.95, .48)
+		p2 = (4.04, .48)
+		p3 = (1.95, 3.88)
+		p4 = (4.05, 3.88)
+		c1 = (1.90, 2.22)
+		c2 = (4.15, 2.15)
+		r1 = 1.65
+		r2 = 1.70
+	if (pos.x < c1[0]):
+		angle = math.atan((pos.y - c1[1])/(pos.x - c1[0]))
+		diff = distance / (2 * math.pi * r1)
+		angle_diff = diff * 2 * math.pi
+		angle_res = angle + angle_diff + math.pi
+		if angle_res < (3 * math.pi / 2):
+			return (c1[0] + r1 * math.cos(angle_res), c1[1] + r1 * math.sin(angle_res))
+		else:
+			new_diff = angle_diff - (angle_res - (3 * math.pi / 2))
+			diff = new_diff / angle_diff
+			distance = distance - (diff * distance)
+			return (p1[0] + distance, p1[1])
+	elif (pos.x < c2[0]) and (pos.y < c2[1]):
+		if pos.x + distance < c2[0]:
+			return (pos.x + distance, p1[1])
+		else:
+			distance = pos.x + distance - c2[0]
+			angle = 3 * math.pi / 2
+			diff = distance / (2 * math.pi * r2)
+			angle_diff = diff * 2 * math.pi
+			angle_res = angle + angle_diff
+			return (c2[0] + r2 * math.cos(angle_res), c2[1] + r2 * math.sin(angle_res))
+	elif (pos.x < c2[0]) and (pos.y >= c2[1]):
+		if pos.x - distance >= p3[0]:
+			return (pos.x - distance, p3[1])
+		else:
+			distance = c1[0] - (pos.x - distance)
+			angle = math.pi / 2
+			diff = distance / (2 * math.pi * r1)
+			angle_diff = diff * 2 * math.pi
+			angle_res = angle + angle_diff
+			return (c1[0] + r1 * math.cos(angle_res), c1[1] + r1 * math.sin(angle_res))
+	elif (pos.x <= 6.00):
+		angle = math.atan((pos.y - c2[1])/(pos.x - c2[0]))
+		diff = distance / (2 * math.pi * r2)
+		angle_diff = diff * 2 * math.pi
+		angle_res = angle + angle_diff
+		if angle_res < (math.pi / 2):
+			return (c2[0] + r2 * math.cos(angle_res), c2[1] + r2 * math.sin(angle_res))
+		else:
+			new_diff = angle_diff - (angle_res - (math.pi / 2))
+			diff = new_diff / angle_diff
+			distance = distance - (diff * distance)
+			return (p4[0] - distance, p4[1])
+
+def on_odom(msg):
+	global ax
+	pos = msg.pose.pose.position
+	(r, p, y) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+	if plot:
+		prepareVisualization()
+	res = getClosestPoint(pos, 0.5, laneID)
+	angle = math.atan2((res[1] - pos.y), (res[0] - pos.x))
+	#print("Yaw: ", y)
+	#print("Target: ", angle)
+	angle_diff = math.atan2(math.sin(angle - y), math.cos(angle - y)) * (-1)
+	map_steering(angle_diff)
+	if plot:
+		ax.plot(pos.x, pos.y, marker='o')
+		ax.plot([pos.x, pos.x + 0.2 * math.cos(y)], [pos.y, pos.y + 0.2 * math.sin(y)], color='g')
+		ax.plot(res[0], res[1], marker='o', color='r')
+		plt.show(block=False)
+		rospy.sleep(0.01)
+
+def on_lane(msg):
+	global laneID
+	laneID = msg.data
+
+def prepareVisualization():
+	global ax
+	angle = 90
+	if ax is None:
+		ax = plt.gca()
+	ax.cla()
+	theta1, theta2 = angle, angle + 180
+	w1 = Wedge((1.95, 2.15), 1.20, theta1, theta2, fill=False)
+	w2 = Wedge((4.05, 2.15), 1.20, theta2, theta1, fill=False)
+	w3 = Wedge((1.95, 2.15), 1.84, theta1, theta2, fill=False)
+	w4 = Wedge((4.05, 2.15), 1.84, theta2, theta1, fill=False)
+	w5 = Wedge((1.95, 2.15), 1.36, theta1, theta2, fill=False, color='b')
+	w6 = Wedge((4.05, 2.15), 1.36, theta2, theta1, fill=False, color='b')
+	w7 = Wedge((1.95, 2.15), 1.68, theta1, theta2, fill=False, color='b')
+	w8 = Wedge((4.05, 2.15), 1.68, theta2, theta1, fill=False, color='b')
+	for wedge in [w1, w2, w3, w4, w5, w6, w7, w8]:
+		ax.add_artist(wedge)
+	ax.set_xlim([0.,6.00])
+	ax.set_ylim([0.,4.30])
+	ax.plot([1.95,4.05], [.95, .95], color='k')
+	ax.plot([1.95,4.05], [.31, .31], color='k')
+	ax.plot([1.95,4.05], [3.36, 3.36], color='k')
+	ax.plot([1.95,4.05], [4.00, 4.00], color='k')
+	ax.plot([1.95,4.05], [.46, .46], color='b')
+	ax.plot([1.95,4.05], [3.84, 3.84], color='b')
+	ax.plot([1.95,4.05], [.78, .78], color='b')
+	ax.plot([1.95,4.05], [3.52, 3.52], color='b')
+
+def main(args):
+	global ax
+	global steering_pub
+	rospy.init_node("navigation", anonymous=True);
+	fig, ax = plt.subplots()
+	steering_pub = rospy.Publisher("/steering", UInt8, queue_size=100, latch=True)
+	pos_sub = rospy.Subscriber("/localization/odom/7", Odometry, on_odom, queue_size=1)
+	lane_sub = rospy.Subscriber("/lane", UInt8, on_lane, queue_size=1)
+	if plot:
+		plt.show()
+	else:
+		rospy.spin()
+
+def map_steering(angle_diff):
+	steering = 2.5 * angle_diff * 180 / math.pi + 90
+	steering = np.clip(steering, 0, 180)
+	steering_pub.publish(steering)
+
+if __name__ == '__main__':
+	main(sys.argv)
